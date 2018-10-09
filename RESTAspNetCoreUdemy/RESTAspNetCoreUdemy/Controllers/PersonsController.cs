bd5dd53640558f6c3e6bf9b78cf0b546301efcd7
Ricardo Dias <cadu.hd@gmail.com>
2018-10-08 21:18:03 -0300

    add db context configs

---
+++ RESTAspNetCoreUdemy/RESTAspNetCoreUdemy/Controllers/PersonsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTAspNetCoreUdemy.Model;
+using RESTAspNetCoreUdemy.Services;
+using System.Collections.Generic;
+
+namespace RESTAspNetCoreUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        public IPersonService _personService;
+
+        public PersonsController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        // GET: api/Person
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        // POST: api/Person
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+            return new ObjectResult(_personService.Create(person));
+        }
+
+        // PUT: api/Person/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest();
+            }
+            return new ObjectResult(_personService.Update(person));
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ RESTAspNetCoreUdemy/RESTAspNetCoreUdemy/Services/Implementations/PersonService.cs
@@ -0,0 +1,67 @@
+﻿using RESTAspNetCoreUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RESTAspNetCoreUdemy.Services.Implementations
+{
+    public class PersonService : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for (int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+        private Person MockPerson(int id)
+        {
+            return new Person()
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name " + id,
+                LastName = "Person LastName " + id,
+                Address = "Avendia xyz, 345",
+                Gender = "Masculino"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person()
+            {
+                Id = 1,
+                FirstName = "Ricardo",
+                LastName = "Dias",
+                Address = "Avendia xyz, 345",
+                Gender = "Masculino"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+    }
+}
